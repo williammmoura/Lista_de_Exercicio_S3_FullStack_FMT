24f84f10f5b3831a9c9873ffa90c207c80c6b379
William Monte de Moura <williamonte@gmail.com>
2023-02-26 16:32:24 -0300

    Exercício 01 - S3

---
+++ script.js
@@ -0,0 +1,10 @@
+/*Exercício 01
+*   Verificar se o número da váriável "num" é par ou ímpar (exibindo o resultado no console).
+*/
+let num = 1;
+
+if(num % 2 == 0){
+    console.log(`O número ${num} é par.`);
+}else{
+    console.log(`O número ${num} é ímpar.`);
+};
