1c6a9b544f22e598da5dca9b480aa989147759dd
William Monte de Moura <williamonte@gmail.com>
2023-02-26 20:39:01 -0300

    Exercício 05 S3

---
+++ script5.js
@@ -0,0 +1,11 @@
+/**
+ * Exercício 05
+ * Escreva um programa que verifique a validade de uma senha fornecida pelo usuário. A senha válida é o número 1234. Devem ser impressas as seguintes mensagens: ACESSO PERMITIDO caso a senha seja válida. ACESSO NEGADO caso a senha seja inválida.
+ */
+let senha = 1284;
+
+if(senha == 1234){
+    console.log(`Senha confirmada`);
+}else{
+    console.log(`ACESSO NEGADO`);
+};
