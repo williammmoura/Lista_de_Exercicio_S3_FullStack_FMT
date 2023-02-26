2ccfae22eb662afbe6248abe28c429fe298e76c3
William Monte de Moura <williamonte@gmail.com>
2023-02-26 20:50:19 -0300

    Exercício 06 S3

---
+++ script6.js
@@ -0,0 +1,12 @@
+/**
+ * Exercício 6
+ * Escreva um programa para ler 2 valores (considere que não serão informados valores iguais) e escrever o maior deles.
+ */
+let valor1 = 3;
+let valor2 = 5;
+
+if(valor1 > valor2){
+    console.log(`Entre ${valor1} e ${valor2} o maior valor é: ${valor1}`);
+}else{
+    console.log(`Entre ${valor1} e ${valor2} o maior valor é: ${valor2}`);
+};
