c4825fd56e7545a9cec1f9ccb63b93e5d39f24b8
William Monte de Moura <williamonte@gmail.com>
2023-02-26 17:52:23 -0300

    Exercício 02 - S2

---
+++ script2.js
@@ -0,0 +1,13 @@
+let idade = 30;
+
+if(idade >=0 && idade <= 12){
+    console.log(`Você tem ${idade} ano(s), logo você é uma CRIANÇA.`);
+}else if(idade > 12 && idade <= 17){
+    console.log(`Você têm ${idade} anos, logo você é um(a) ADOLESCENTE.`);
+}else if(idade > 17 && idade <= 65){
+    console.log(`Você têm ${idade} anos, logo você é um(a) ADULTO(A).`);
+}else if(idade >= 66 && idade <= 110){
+    console.log(`Você têm ${idade} anos, logo você é um(a) SÊNIOR.`);
+}else{
+    console.log(`Valor INVÁLIDO!`);
+};
